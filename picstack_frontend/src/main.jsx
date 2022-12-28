8cac6e6f63422d98ad51838acd0d9c0f79c9ee94
Karan Singh <karanrsingh2662006@gmail.com>
2022-12-28 21:24:21 +0530

    minor issue solved

--- picstack_frontend/src/main.jsx
+++ picstack_frontend/src/main.jsx
@@ -8,7 +8,7 @@ import {GoogleOAuthProvider} from '@react-oauth/google';
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <Router>
-      <GoogleOAuthProvider clientId='505631087154-9iv60lbd8lmp8b9rh16h5bhheaakqm2q.apps.googleusercontent.com'>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_API_TOKEN}>
     <App />
     </GoogleOAuthProvider>
     </Router>
